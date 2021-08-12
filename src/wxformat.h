4426db9fa27664ea452463cc5e0d23f9c532bda1
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-08-12 18:18:01 +0200

    fix fmt

--- src/wxformat.h
+++ src/wxformat.h
@@ -7,9 +7,9 @@
 #include <wx/string.h>
 
 namespace intl {
-    template<typename ... Ts>
-    inline wxString wxformat(const char *fmt_str, Ts && ... args) {
-        std::string str = format(fmt_str, std::forward<Ts>(args) ...);
+    template<typename T, typename ... Ts>
+    inline wxString wxformat(T &&fmt_str, Ts && ... args) {
+        std::string str = format(std::forward<T>(fmt_str), std::forward<Ts>(args) ...);
         return wxString::FromUTF8(str.data(), str.size());
     }
 }
@@ -17,7 +17,7 @@ namespace intl {
 namespace enums {
     template<enums::is_enum T>
     inline auto get_label(T num) {
-        return intl::wxformat(fmt::format("{}::{}", magic_enum::enum_type_name<T>(), magic_enum::enum_name(num)).c_str());
+        return intl::wxformat(std::format("{}::{}", magic_enum::enum_type_name<T>(), magic_enum::enum_name(num)));
     }
 }
 
